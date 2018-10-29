e1621a9c844793381451efd7964619e0403a8b66
aritra-b-11 <aritrabhattacharjee12@gmail.com>
2018-10-29 23:55:03 +0530

    no package added, just some testing done for some package

--- init.el
+++ init.el
@@ -17,10 +17,11 @@
     ("aaffceb9b0f539b6ad6becb8e96a04f2140c8faa1de8039a343a4f1e009174fb" "0bff60fb779498e69ea705825a2ca1a5497a4fccef93bf3275705c2d27528f2f" default)))
  '(package-selected-packages
    (quote
-    (magit counsel-projectile counsel ivy org-bullets cycle-themes dracula-theme exotica-theme ido-grid-mode ido-describe-bindings))))
+    (paredit iedit magit counsel-projectile counsel ivy org-bullets cycle-themes dracula-theme exotica-theme ido-grid-mode ido-describe-bindings))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'narrow-to-region 'disabled nil)
